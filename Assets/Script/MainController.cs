bdd3209199efa7936372a6e6dc853439cecfea56
luma <stubma@gmail.com>
2016-11-01 00:28:52 +0800

    start to work

--- Assets/Script/MainController.cs
+++ Assets/Script/MainController.cs
@@ -1,37 +1,62 @@
 ﻿using UnityEngine;
 using System.Collections;
 using HoloToolkit.Unity;
+using UnityEngine.UI;
 
 public class MainController : MonoBehaviour {
-	// lowest table
-	private GameObject table;
+	[Tooltip("A canvas which holds hint message text")]
+	public Canvas hintCanvas;
 
-	// Use this for initialization
+	[Tooltip("A text control which displays hint message")]
+	public Text hintText;
+
+	// canvas distance
+	private float hintCanvasDistance;
+
+	/// <summary>
+	/// current state of app 
+	/// </summary>
+	private enum OpState {
+		IDLE = 0,
+		LOCATE_SURFACE_BOOK = 1,
+		LOCATE_NEOBOX = 2
+	}
+
+	/// <summary>
+	/// state of app
+	/// </summary>
+	private OpState state;
+
 	void Start () {
-		// register for plane completed event 
-		SurfaceMeshesToPlanes.Instance.MakePlanesComplete += SurfaceMeshesToPlanes_MakePlanesComplete;  
+		// init state
+		setState(OpState.LOCATE_SURFACE_BOOK);
+
+		// distance of hint canvas
+		hintCanvasDistance = hintCanvas.transform.position.magnitude;
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
-	
+		// let hint canvas follow camera
+		if(hintCanvas.gameObject.activeSelf) {
+			hintCanvas.transform.position = gameObject.transform.forward * hintCanvasDistance;
+			hintCanvas.transform.localRotation = gameObject.transform.localRotation;
+		}
 	}
 
-	/// <summary>  
-	/// unregister event handler when destory
-	/// </summary>  
-	private void OnDestroy() {  
-		if(SurfaceMeshesToPlanes.Instance != null) {  
-			SurfaceMeshesToPlanes.Instance.MakePlanesComplete -= SurfaceMeshesToPlanes_MakePlanesComplete;  
-		}  
-	}
-
-	private void SurfaceMeshesToPlanes_MakePlanesComplete(object source, System.EventArgs args) {  
-		// show lowest horizontal plane
-		table = PlanesManager.Instance.GetLowestHorizontalPlane();
-		if(table != null) {
-			SurfacePlane plane = table.GetComponent<SurfacePlane>();
-			plane.IsVisible = true;
+	private void setState(OpState s) {
+		state = s;
+		switch(state) {
+		case OpState.LOCATE_SURFACE_BOOK:
+			hintCanvas.gameObject.SetActive(true);
+			hintText.text = "Locate Your Surface Book";
+			break;
+		case OpState.LOCATE_NEOBOX:
+			hintCanvas.gameObject.SetActive(true);
+			hintText.text = "Locate Neobox";
+			break;
+		default:
+			hintCanvas.gameObject.SetActive(false);
+			break;
 		}
 	}
 }
