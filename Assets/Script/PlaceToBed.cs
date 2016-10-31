6069c4ec87adcaad17dc250f2089b9d7e5ea8a5d
luma <stubma@gmail.com>
2016-10-31 21:58:20 +0800

    fix bear place to bed

--- Assets/Script/PlaceToBed.cs
+++ Assets/Script/PlaceToBed.cs
@@ -5,10 +5,11 @@ using UnityEngine.UI;
 public class PlaceToBed : MonoBehaviour {
 
     public Text MessageText;
+	private GameObject bed;
 
     // Use this for initialization
     void Start () {
-	
+		bed = GameObject.Find("Bed");
 	}
 	
 	// Update is called once per frame
@@ -17,28 +18,24 @@ public class PlaceToBed : MonoBehaviour {
 	}
 
     void OnSelect()
-    {
+    {	
         GameObject duplicate = Instantiate<GameObject>(gameObject);
         Destroy(duplicate.GetComponent<PlaceToBed>());
-        GameObject bed = GameObject.Find("Bed");
+
         Bounds bounds = duplicate.GetComponent<Renderer>().bounds;
         //bounds.min
         Vector3 modelBottomCenter = bounds.center;
         modelBottomCenter.y = bounds.min.y;
-
         Bounds bedBounds = bed.GetComponent<Renderer>().bounds;
         Vector3 bedTopCenter = bedBounds.center;
         bedTopCenter.y = bedBounds.min.y;
-
         duplicate.transform.position = duplicate.transform.position + (bedTopCenter - modelBottomCenter);
 
         duplicate.transform.parent = bed.transform;
 
         //duplicate.GetComponent<BoxCollider>().enabled = false;
-
         Interaction interaction = duplicate.AddComponent<Interaction>();
         interaction.MessageText = MessageText;
-
         TargetManager.Instance.SetTarget(duplicate);
     }
 }
