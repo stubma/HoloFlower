13775ca3f672104b3ff0c6346e7d0544c38e9c86
luma <stubma@gmail.com>
2016-11-10 21:03:34 +0800

    pretend print

---
+++ Assets/Script/NeoboxController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NeoboxController : MonoBehaviour {
+	[Tooltip("A hint canvas used to display hint message")]
+	public GameObject hintCanvas;
+
+	void Start () {
+		// place hint canvas
+		RectTransform tf = hintCanvas.GetComponent<RectTransform>();
+		hintCanvas.transform.localPosition = new Vector3(0, tf.rect.height / 2 + 0.01f, 0);
+
+		// hide hint canvas
+		hintCanvas.SetActive(false);
+	}
+
+	public void Print() {
+		// show hint canvas
+		hintCanvas.SetActive(true);
+	}
+}
--- Assets/Script/PrintButtonHandler.cs
+++ Assets/Script/PrintButtonHandler.cs
@@ -3,5 +3,11 @@ using System.Collections;
 
 public class PrintButtonHandler : MonoBehaviour {
 	void OnSelect() {
+		// get neobox controller
+		MainController mc = Camera.main.GetComponent<MainController>();
+		NeoboxController nc = mc.neoboxPlaceholder.GetComponent<NeoboxController>();
+
+		// print
+		nc.Print();
 	}
 }
