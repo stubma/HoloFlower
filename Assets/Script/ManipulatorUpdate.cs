804940b28f7ce05cabb570bf564c40f9700a92b7
luma <stubma@gmail.com>
2016-11-10 20:29:13 +0800

    rotation ok

--- Assets/Script/ManipulatorUpdate.cs
+++ Assets/Script/ManipulatorUpdate.cs
@@ -2,29 +2,27 @@
 using System.Collections;
 
 public class ManipulatorUpdate : MonoBehaviour {
-	// is manipulator placed
-	private bool isManipulatorPlaced = false;
-
 	void Update() {
 		if(TargetManager.Instance.Target != null) {
-			if(!isManipulatorPlaced) {
-				// set target as manipulator's parent
-				gameObject.transform.parent = TargetManager.Instance.Target.transform;
+			// get flower controller
+			MainController mc = Camera.main.GetComponent<MainController>();
+			SBPlaceholderController sbpc = mc.surfaceBookPlaceholder.GetComponent<SBPlaceholderController>();
+			FlowerController fc = sbpc.flowerBox.GetComponent<FlowerController>();
 
-				// get flower controller
-				MainController mc = Camera.main.GetComponent<MainController>();
-				SBPlaceholderController gc = mc.surfaceBookPlaceholder.GetComponent<SBPlaceholderController>();
-				FlowerController fc = gc.flowerBox.GetComponent<FlowerController>();
+			// set manipulator position
+			Vector3 pos = sbpc.flowerBox.transform.position;
+			pos.y += fc.FlowerBound.size.y / 2;
+			gameObject.transform.position = pos;
 
-				// set manipulator position
-				gameObject.transform.localPosition = new Vector3(0, fc.FlowerBound.size.y / 2, 0);
+			// scale by target bound
+			Vector3 scale = Vector3.Scale(sbpc.flowerBox.transform.localScale, fc.FlowerBound.size);
+			gameObject.transform.localScale = scale;
 
-				// scale by target bound
-				gameObject.transform.localScale = fc.FlowerBound.size;
-
-				// set flag
-				isManipulatorPlaced = true;
-			}
+			// rotation
+			Quaternion rotation = sbpc.flowerBox.transform.localRotation;
+			rotation.x = 0;
+			rotation.z = 0;
+			gameObject.transform.localRotation = rotation;
 		}
 	}
 }
