99b9af5394a9c7fe005319451e344fb998c92c80
luma <stubma@gmail.com>
2016-11-12 10:25:38 +0800

    fix rotation

--- Assets/Script/RotateButtonHandler.cs
+++ Assets/Script/RotateButtonHandler.cs
@@ -15,9 +15,16 @@ public class RotateButtonHandler : MonoBehaviour {
 		SBController sbc = mc.surfaceBookPlaceholder.GetComponent<SBController>();
 		FlowerController fc = sbc.flowerBox.GetComponent<FlowerController>();
 
-		// calculate start and end rotation
+		// axis is in world space and not transformed, so we need transform it
+		// with camera y roation first, then convert it to flower space
+		Quaternion axisRotate = Quaternion.Euler(new Vector3(0, Camera.main.transform.localRotation.eulerAngles.y, 0));
+		Vector3 globalPoint = axisRotate * axis;
+		globalPoint += sbc.flowerBox.transform.position;
+		Vector3 localAxis = sbc.flowerBox.transform.InverseTransformPoint(globalPoint);
+
+		// calculate rotation by local axis
 		Quaternion startRotation = sbc.flowerBox.transform.localRotation;
-		Quaternion endRotation = startRotation * Quaternion.AngleAxis(angle, axis);
+		Quaternion endRotation = startRotation * Quaternion.AngleAxis(angle, localAxis);
 
 		// call flower controller method
 		fc.Rotate(startRotation, endRotation);
